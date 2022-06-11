4c2f7f060cc20af711115d814a2a647d52c8106b
Robert Pylkkänen <robert.pylkkanen@gmail.com>
2022-06-11 11:24:39 +0300

    rcparams

--- matplotlib_style/rcparams.py
+++ matplotlib_style/rcparams.py
@@ -1,3 +1,12 @@
 import matplotlib
 
 matplotlib.rcParams['lines.linewidth'] = matplotlib.rcParams['axes.linewidth']
+matplotlib.rcParams['patch.linewidth'] = matplotlib.rcParams['axes.linewidth']
+matplotlib.rcParams['hatch.linewidth'] = matplotlib.rcParams['axes.linewidth']
+matplotlib.rcParams['boxplot.flierprops.linewidth'] = matplotlib.rcParams['axes.linewidth']
+matplotlib.rcParams['boxplot.boxprops.linewidth'] = matplotlib.rcParams['axes.linewidth']
+matplotlib.rcParams['boxplot.whiskerprops.linewidth'] = matplotlib.rcParams['axes.linewidth']
+matplotlib.rcParams['boxplot.capprops.linewidth'] = matplotlib.rcParams['axes.linewidth']
+matplotlib.rcParams['boxplot.medianprops.linewidth'] = matplotlib.rcParams['axes.linewidth']
+matplotlib.rcParams['boxplot.meanprops.linewidth'] = matplotlib.rcParams['axes.linewidth']
+matplotlib.rcParams['grid.linewidth'] = matplotlib.rcParams['axes.linewidth']
