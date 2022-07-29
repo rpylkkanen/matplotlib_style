4d63c5aa7ca95ba0a7b6531eac6e35b234daf3db
rpylkkanen <robert.pylkkanen@gmail.com>
2022-07-29 20:31:27 +0300

    styling

--- matplotlib_style/rcparams.py
+++ matplotlib_style/rcparams.py
@@ -1,6 +1,11 @@
 import matplotlib
+from cycler import cycler
 
+# blue, orange, green, red, purple, brown, pink, grey, yellow, teal
+matplotlib.rcParams['axes.prop_cycle'] = cycler('color', ['#2196f3', '#ff9800', '#4caf50', '#f44336'])
 matplotlib.rcParams['lines.linewidth'] = matplotlib.rcParams['axes.linewidth']
+matplotlib.rcParams['lines.markersize'] = 4.5
+matplotlib.rcParams['errorbar.capsize'] = matplotlib.rcParams['lines.markersize'] / 2
 matplotlib.rcParams['patch.linewidth'] = matplotlib.rcParams['axes.linewidth']
 matplotlib.rcParams['hatch.linewidth'] = matplotlib.rcParams['axes.linewidth']
 matplotlib.rcParams['boxplot.flierprops.linewidth'] = matplotlib.rcParams['axes.linewidth']
