71c1fc4f1ef46866113b0f7debd0ea9b4f7cd3c2
rpylkkanen <robert.pylkkanen@gmail.com>
2024-08-07 16:58:05 +0300

    rcparams

--- matplotlib_style/rcparams.py
+++ matplotlib_style/rcparams.py
@@ -19,15 +19,6 @@ matplotlib.rcParams['ytick.right']          = False
 # blue, orange, green, red, purple, brown, pink, grey, yellow, teal
 matplotlib.rcParams['axes.prop_cycle'] = cycler('color', ['#d32f2f', '#1976d2', '#388e3c', '#f57c00', '#7b1fa2', '#fbc02d'])
 
-matplotlib.rcParams['lines.markersize'] = 4.0
-matplotlib.rcParams['errorbar.capsize'] = matplotlib.rcParams['lines.markersize'] / 2
-matplotlib.rcParams['lines.markerfacecolor'] = 'white'
-matplotlib.rcParams['lines.markeredgewidth'] = matplotlib.rcParams['axes.linewidth']
-matplotlib.rcParams['xtick.major.size'] = matplotlib.rcParams['lines.markersize']
-matplotlib.rcParams['xtick.minor.size'] = matplotlib.rcParams['lines.markersize'] / 2
-matplotlib.rcParams['ytick.major.size'] = matplotlib.rcParams['lines.markersize']
-matplotlib.rcParams['ytick.minor.size'] = matplotlib.rcParams['lines.markersize'] / 2
-
 matplotlib.rcParams['axes.linewidth'] = 0.5
 matplotlib.rcParams['xtick.major.width'] = matplotlib.rcParams['axes.linewidth']
 matplotlib.rcParams['xtick.minor.width'] = matplotlib.rcParams['axes.linewidth']
@@ -43,5 +34,21 @@ matplotlib.rcParams['boxplot.capprops.linewidth'] = matplotlib.rcParams['axes.li
 matplotlib.rcParams['boxplot.medianprops.linewidth'] = matplotlib.rcParams['axes.linewidth']
 matplotlib.rcParams['boxplot.meanprops.linewidth'] = matplotlib.rcParams['axes.linewidth']
 matplotlib.rcParams['grid.linewidth'] = matplotlib.rcParams['axes.linewidth']
+
+matplotlib.rcParams['lines.markersize'] = 3 + 2/3
+matplotlib.rcParams['errorbar.capsize'] = matplotlib.rcParams['lines.markersize'] * 2/3
+matplotlib.rcParams['lines.markerfacecolor'] = 'white'
+matplotlib.rcParams['lines.markeredgewidth'] = matplotlib.rcParams['axes.linewidth']
+matplotlib.rcParams['xtick.major.size'] = matplotlib.rcParams['lines.markersize']
+matplotlib.rcParams['xtick.minor.size'] = matplotlib.rcParams['lines.markersize'] / 2
+matplotlib.rcParams['ytick.major.size'] = matplotlib.rcParams['lines.markersize']
+matplotlib.rcParams['ytick.minor.size'] = matplotlib.rcParams['lines.markersize'] / 2
+matplotlib.rcParams['axes.titlesize'] = 'medium'
+matplotlib.rcParams['axes.labelpad'] = 0
+matplotlib.rcParams['axes.titlepad'] = 4
+matplotlib.rcParams['xtick.major.pad'] = 2
+matplotlib.rcParams['xtick.minor.pad'] = 2
+matplotlib.rcParams['ytick.major.pad'] = 2
+matplotlib.rcParams['ytick.minor.pad'] = 2
 
 matplotlib.rcParams['path.snap'] = False
